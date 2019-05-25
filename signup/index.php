b6ba9a5189d485f6eab45df8548dbe50b8d011c1
UBC Student <gary1999@lulu.students.cs.ubc.ca>
2019-05-25 00:46:43 -0700

    change signup page

--- signup/index.php
+++ signup/index.php
@@ -24,45 +24,45 @@
 <div style="height: 100px"></div>
 
 
-	<?php 
-	$ispost = false;
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		print_r($_POST);
-		$ispost = true;
-	}
+<?php 
+$ispost = false;
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	#print_r($_POST);
+	$ispost = true;
+}
 
-	if(/*$_SERVER["REQUEST_METHOD"] == "POST"*/false) {
+if(/*$_SERVER["REQUEST_METHOD"] == "POST"*/false) {
       // username and password sent from form 
 
-		$conn = OCILogon ("ora_gary1999", 'a42252965', "dbhost.students.cs.ubc.ca:1522/stu");
-		if (!$conn) {
-			echo "ERROR";
-		}
-		$stid = OCIParse($conn, 'INSERT INTO Attractions_InsepectAndDeterminesStatus');
-		$myusername = $_POST['username'];
-		$mypassword = $_POST['password']; 
-		if (!$stid) {
-			echo "<br>Cannot parse this command: ". "<br>";
-			$e = OCI_Error($conn); 
-           // For OCIParse errors, pass the connection handle.
-			echo htmlentities($e['message']);
-			$success = False;
-		}
-
-		$r = OCIExecute($stid, OCI_DEFAULT);
-		if (!$r) {
-			echo "<br>Cannot execute this command: " . $cmdstr . "<br>";
-			$e = oci_error($statement); 
-           // For OCIExecute errors, pass the statement handle.
-			echo htmlentities($e['message']);
-			$success = False;
-		} else {
-		}
-
+	$conn = OCILogon ("ora_gary1999", 'a42252965', "dbhost.students.cs.ubc.ca:1522/stu");
+	if (!$conn) {
+		echo "ERROR";
 	}
-	
+	$stid = OCIParse($conn, 'INSERT INTO Attractions_InsepectAndDeterminesStatus');
+	$myusername = $_POST['username'];
+	$mypassword = $_POST['password']; 
+	if (!$stid) {
+		echo "<br>Cannot parse this command: ". "<br>";
+		$e = OCI_Error($conn); 
+           // For OCIParse errors, pass the connection handle.
+		echo htmlentities($e['message']);
+		$success = False;
+	}
 
-	?>
+	$r = OCIExecute($stid, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute this command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); 
+           // For OCIExecute errors, pass the statement handle.
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+	}
+
+}
+
+
+?>
 
 
 
@@ -73,17 +73,40 @@
 	<label>Adults : </label>  
 	<button type = button id = "addAdult">Add Adult</button>
 	<table class = "adultsTable">
-	<?php
-	if($ispost){
-	foreach($_POST["adults"] as $add){
-		echo "<tr><td><input type='text' name='adults[]' placeholder='Name of an adult' value = '$add'></td></tr>";
-	}
-}
+		<?php
+		if($ispost){
+			foreach($_POST["adults"] as $add){
+				echo "<tr><td><input type='text' name='adults[]' placeholder='Name of an adult' value = '$add'></td></tr>";
+			}
+		}else{
+			echo "<tr><td><input type='text' name='adults[]' placeholder='Name of an adult' value = '$add'></td></tr>";
+		}
 
-	?>
+		?>
 
-</table>
-	<label>Children: </label> <input type="text" name="children[]"> <input type="text" name = "responsible[]">
+	</table>
+	<label>Children: </label>
+	<button type = button id = "addChildren">Add Children</button>
+	<table class = "childrenTable">
+		<?php
+		if($ispost){
+			for ($i = 0; $i < count($_POST["children"]); $i++) {
+				$chd = $_POST['children'][$i];
+				$res = $_POST['responsible'][$i];
+				echo " <tr><td><input type='text' 
+				name='children[]' 
+				placeholder = 'Name Of the Children' 
+				value = '$chd'> </td> 
+				<td> <input type='text' name = 'responsible[]' placeholder= 'Name of the Responsible Adult' value = '$res'></td></tr>";
+			}
+		}
+
+		?>
+
+	</table>
+
+
+	
 	<input type = "submit" value = " Submit "/><br />
 </form>
 
@@ -94,12 +117,23 @@
 var $deletething = $('.del_thing');
 
 $('#addAdult').click(function(){
-  $thing_table = $('.adultsTable');
+	$thing_table = $('.adultsTable');
   // if input is empty, it won't add an empty row
     // new thing is added to end of table
     // change to prepend to add to the beginning of table
     $thing_table.append("<tr><td><input type='text' name='adults[]' placeholder='Name of an adult'></td></tr>");
-  
+
+  // empties the input when sumbit is clicked
+
+});
+
+$('#addChildren').click(function(){
+	$thing_table = $('.childrenTable');
+  // if input is empty, it won't add an empty row
+    // new thing is added to end of table
+    // change to prepend to add to the beginning of table
+    $thing_table.append("<tr><td><input type='text' name='children[]' placeholder = 'Name Of the Children' value = ''></td><td> <input type='text' name = 'responsible[]' placeholder= 'Name of the Responsible Adult' value = ''></td><tr>");
+
   // empties the input when sumbit is clicked
 
 });
