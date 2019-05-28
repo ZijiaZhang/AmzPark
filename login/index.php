193add2df806bbdd3bb37cf13005e1d8477cf1a4
zijia zhang <zijia.zhang99@gmail.com>
2019-05-28 16:55:35 -0700

    Add css for login and signup page

--- login/index.php
+++ login/index.php
@@ -10,6 +10,7 @@
 	<meta charset="UTF-8">
 	<title> Amz Park</title>
 	<link rel="stylesheet" type = "text/css" href="../server_files/css/mycss.css">
+	<link rel="stylesheet" type = "text/css" href="../server_files/css/form.css">
 	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
 	<link rel="stylesheet" href="https://cdn.materialdesignicons.com/3.6.95/css/materialdesignicons.min.css">
 	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
@@ -30,13 +31,13 @@ $(function(){
 <div style="height: 100px"></div>
 
 
-
+<div class = "formContainer">
 <form action ="" method="post">
-	<label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input name = "submit" type = "submit" value = "login "/><br />
+	<input type = "text" name = "username" class = "box" placeholder="Username" /><br /><br />
+    <input type = "password" name = "password" class = "box" placeholder="Password" /><br/><br />
+    <input name = "submit" type = "submit" value = "LOGIN "/><br />
 </form>
-
+</div>
 
 
 </html>
