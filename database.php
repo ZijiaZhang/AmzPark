a5690fdadecfd30de27716c0dedf738d1ff3dad7
Zijia Zhang <Zijia.Zhang99@gmail.com>
2019-05-30 14:35:17 -0700

    User Can now add or Delete Adults

--- database.php
+++ database.php
@@ -21,7 +21,7 @@ function executeSQL($command){
            // For OCIExecute errors, pass the statement handle.
 		#echo htmlentities();
 		#$success = False;
-		throw new Exception('Cannot execute this command'.$e['message']);
+		throw new Exception('Cannot execute this command'.$command.$e['message']);
 	} else {
 									#echo "<br>Command: success " .$command. "<br>";
 	}
@@ -41,6 +41,36 @@ function insertInto($item, $database){
 	$command = "INSERT INTO $database VALUES ($item)";
 	#echo $command;
 	$stid = executeSQL($command);
+}
+
+function getMember($GroupID){
+	$result = executeSQL("SELECT * FROM groups WHERE GROUPID = '$GroupID'");
+	$row = oci_fetch_array($result);
+	$groupSize = $row['GROUPSIZE'];
+	#var_dump($groupSize);
+
+	$adults = array();
+	$adt = executeSQL("SELECT * FROM AdultVisitor_include WHERE  groupID = '$GroupID'");
+	
+	while ($adult =  oci_fetch_array($adt)) {
+		array_push($adults, $adult);
+	}
+	
+	$Children = array();
+	$chd = executeSQL("SELECT * FROM AdultVisitor_include WHERE  groupID = '$GroupID'");
+	
+	while ($child =  oci_fetch_array($chd)) {
+		array_push($Children, $child);
+	}
+
+	$Children = oci_fetch_array(executeSQL("SELECT * FROM YoungVisitor_include_isGuradedBy WHERE  younggroupID = '$GroupID'"));
+
+	return array('GS'=>$groupSize,'AD'=>$adults,'CH'=>$Children);
+}
+
+function updateGroupSize($GroupID){
+	executeSQL("UPDATE groups SET GROUPSIZE = (SELECT count(*) FROM AdultVisitor_include WHERE  groupID = '$GroupID') WHERE groupID = '$GroupID'");
+
 }
 
 ?>
--- myaccount/index.php
+++ myaccount/index.php
@@ -1,18 +1,165 @@
 <?php
 //include_once '../login.php';
 include_once '../session.php';
+include_once '../database.php';
 initializeSession();
 #print_r($_SESSION);
 
-	if(checkSession()){
+if(checkSession()){
 		//header('location: ./account');
-		echo "Your Session ID: ";
-		var_dump(session_id());
-		$name = $_SESSION['login_user'];
-		echo "Your Username is $name";
-	}else{
-		header('location: ../login');
-	}
+	echo "Your Session ID: ";
+	var_dump(session_id());
+	$name = $_SESSION['login_user'];
+}else{
+	header('location: ../login');
+}
 ?>
 
-<a href="../logout.php">Log Out</a>
+<?php
+var_dump($_POST);
+if(isset($_POST['submit'])){
+	if($_POST['submit']== 'delete_adult'){
+		$Visitername = $_POST['del_visitor'];
+		executeSQL("DELETE FROM AdultVisitor_include where groupID = '$name' and visitorname = '$Visitername'");
+	}else if($_POST['submit'] == 'insert_adult'){
+		$Visitername = $_POST['ins_visitor'];
+		$Contact = $_POST['ins_contact'];
+		try{
+		insertInto("'$Visitername','$name','$Contact'","AdultVisitor_include");
+		}catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+	try{
+		updateGroupSize($name);
+	}catch(Exception $e){
+		echo $e->getMessage();
+	}
+
+}
+
+?>
+<?php
+
+
+	$groupSize = 'ERROR';
+	$Adults = 'ERROR';
+	$Children = 'ERROR';
+	try{
+		$info = getMember($name);
+		var_dump($info);
+		$groupSize = $info['GS'];
+		$Adults = $info['AD'];
+		$Children = $info ['CH'];
+	}catch(EXception $e){
+		echo $e->getMessage();
+	}
+
+
+	echo "Your Username is $name";
+
+
+?>
+
+
+<style>
+#mainContainer{
+	width: 100%;
+	height: 100%;
+	display: flex;
+}
+#operationPannel{
+	background-color: blue;
+	width: 70%;
+	overflow: auto;
+	display: block;
+}
+#info{
+	overflow: auto;
+	background-color: green;
+	width :30%;
+	position: relative;
+	display: block;
+}
+#info p a{
+	color: blue;
+}
+#adultInfo tr td, #adultInfo tr th{
+	border-style: solid;
+	border-width: 2px
+}
+#adultInfo{
+	border-collapse: collapse;
+}
+
+.popup {
+	display:none;
+	position: fixed;
+	z-index: 9;
+	background-color: white;
+}
+</style>
+<div id = "mainContainer">
+	<div id = "info">
+		<p>Your Group Name is <a><?php echo $name ?> </a></p>
+		<p>Your Group Size is <a> <?php echo $groupSize;?> </a></p>
+		<button id="adultPanelButton" onclick="ToggleForm()">Create Adult</button>
+		<div class="popup" id="adultAddform">
+			<form action="" class="form-container" method = "post">
+				<h1>ADD ADULT</h1>
+				<table>
+				<tr>
+					<td>
+				<label for="name"><b>Name</b></label>
+			</td>
+			<td>
+				<input type="text" placeholder="Enter Name" name="ins_visitor" required>
+			</td>
+			</tr>
+				<tr><td>
+				<label for="contact"><b>Contact</b></label></td><td>
+				<input type="text" placeholder="Enter Contact" name="ins_contact" required></td>
+			</tr>
+				</table>
+				<button type="submit" class="" name = "submit" value = 'insert_adult'>Create</button>
+				<button type="button" class="" onclick="closeForm()">Close</button>
+			</form>
+		</div>
+		
+
+		<table id = "adultInfo">
+			<tr><th>Name</th><th>Contact</th><th>delete</th></tr>
+			<?php foreach($Adults as $adult){ ?>
+				<form action = "" method = "post">
+					<tr><td><input type = "hidden" name = "del_visitor" value = <?php echo $adult['VISITORNAME'];?> > <?php echo $adult['VISITORNAME'];?></td>
+						<td> <?php echo $adult['CONTACT_INFO'];?></td>
+						<td><button type="submit" value = "delete_adult" name = "submit">Delete</button>
+						</tr>
+					</form>
+				<?php } ?>
+			</table>
+
+		</div>
+		<div id = "operationPannel">
+			<p>Hello</p>
+		</div>
+	</div>
+	<a href="../logout.php">Log Out</a>
+
+	<script>
+		function ToggleForm() {
+			if(document.getElementById("adultAddform").style.display == "block"){
+				document.getElementById("adultAddform").style.display = "none";
+				document.getElementById("adultPanelButton").InnerHTML = "Create Adult";
+			}
+			else{
+				document.getElementById("adultAddform").style.display = "block";
+				document.getElementById("adultPanelButton").InnerHTML = "Close Window";
+			}
+		}
+
+		function closeForm() {
+			document.getElementById("adultAddform").style.display = "none";
+		}
+	</script>
