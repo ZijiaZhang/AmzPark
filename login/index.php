a5d6ae9ed8fd7b6df1fed06b765b2c1a01e2b4be
Zijia Zhang <Zijia.Zhang99@gmail.com>
2019-06-05 23:41:28 -0700

    fix mobile site

--- login/index.php
+++ login/index.php
@@ -10,6 +10,7 @@
 <html lang="en"><head>
 	<meta charset="UTF-8">
 	<title> Amz Park</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="stylesheet" type = "text/css" href="../server_files/css/mycss.css">
 	<link rel="stylesheet" type = "text/css" href="../server_files/css/form.css">
 	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
