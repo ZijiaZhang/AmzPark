c611baf6e4ebd1a33286c7547162ed5bb3ce29bc
Zijia Zhang <Zijia.Zhang99@gmail.com>
2019-06-16 13:17:02 -0700

    crete massage box to display error messages

--- signup.php
+++ signup.php
@@ -97,7 +97,7 @@ if($ispost) {
 				#Signup Successful
 				header('location: ../login?message=signup');
 			}catch (Exception $e){
-				echo 'Caught exception: ',  $e->getMessage(), "\n";
+				$Message = "Illigal inputs, Please Check the information provided.";//echo 'Caught exception: ',  $e->getMessage(), "\n";
 				$list1 = array(":bind1" => $name ); 
 				executeBoundSQL("DELETE FROM groups WHERE groupID = :bind1" , $list1);
 			}
