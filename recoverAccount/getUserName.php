b6ec80f8673e375740f738311f725a11f19b2f6e
Zijia Zhang <Zijia.Zhang99@gmail.com>
2019-06-17 20:57:58 -0700

    change css for message

--- recoverAccount/getUserName.php
+++ recoverAccount/getUserName.php
@@ -5,15 +5,17 @@
 		margin-top:40vh;
 	}
 	h2{
+		color:#f99500;;
 		text-align: center;
 	}
 </style>
 <div style="width: 100%"; >
-	<h1> Enter Your User Name:</h1>
+	
 	<h2><?php if(isset($_GET["message"])) {
 		if($_GET["message"]=='fail')
 		echo "The information you provided is not correct. Please try again.";
 	}?></h2>
+	<h1> Enter Your User Name:</h1>
 	<form method = "POST">
 		<input type="text" name="accountName" required>
 		<button dis = "normal" type="submit" name="submit" value="confirmInfo"> NEXT</button>
