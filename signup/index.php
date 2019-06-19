dca66c08681afe1121590e68c1950085680140e0
Zijia Zhang <Zijia.Zhang99@gmail.com>
2019-06-19 13:39:36 -0700

    change the titles

--- signup/index.php
+++ signup/index.php
@@ -1,6 +1,6 @@
 <html lang="en"><head>
 	<meta charset="UTF-8">
-	<title> Amz Park</title>
+	<title> Sign Up</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
 	<link rel="stylesheet" href="https://cdn.materialdesignicons.com/3.6.95/css/materialdesignicons.min.css">
